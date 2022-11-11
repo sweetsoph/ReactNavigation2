50021e39bb62db49faf475f07ea7034f572f2b8c
sweetsoph <sophia.verardods@gmail.com>
2022-11-11 00:04:32 -0300

    trying to work but my computer is slow

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,34 +1,43 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   FlatList,
   ScrollView,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
-import { TituloPagina } from "./StyledComponents";
+import backgroundImage from "../src/images/background.png";
+import {
+  TituloPagina,
+  TextDefault,
+  BgTemperature,
+  TxtTemperature,
+} from "./StyledComponents";
 import axios from "axios";
 
 export default function Forecast({ navigation, route }) {
   const [data, setData] = React.useState({});
   useEffect(() => {
-    console.log("cidade", route.params.city);
+    console.log("ai calica");
     axios
       .get(
         `https://api.hgbrasil.com/weather?key=0cf39b5c&city_name=${route.params.city}`
       )
       .then((response) => {
         setData(response.data.results);
+        console.log(data.forecast);
       })
       .catch((error) => {
         console.error("erro", error);
-      }).finally(()=>{
-        console.log("Finalizado");
       })
+      .finally(() => {
+        console.log("Finalizado");
+      });
   }, []);
   return (
-    <View style={{ paddingVertical: 30, paddingHorizontal: 15 }}>
+    <ScrollView style={{ paddingVertical: 30, paddingHorizontal: 15 }}>
       <View
         style={{
           height: 35,
@@ -47,12 +56,32 @@ export default function Forecast({ navigation, route }) {
         </TouchableOpacity>
         <TituloPagina>{data?.city}</TituloPagina>
       </View>
-      <Text>Seja Bem Vindo</Text>
-      <FlatList
-        data={data.forecast}
-        renderItem={({ item }) => <Text>{item.max}</Text>}
-        keyExtractor={(item) => item.date}
-      />
-    </View>
+      <View style={{ alignItems: "center" }}>
+        <View
+          style={{
+            alignItems: "center",
+            position: "relative",
+            marginVertical: 30,
+          }}
+        >
+          <BgTemperature source={backgroundImage} />
+          <TxtTemperature>{data?.temp}°C</TxtTemperature>
+        </View>
+        <View style={{flexDirection:"row"}}>
+          <TextDefault>Previsão dos 10 Próximos Dias</TextDefault>
+          <Icon name={"chevron-forward"} size={20}></Icon>
+        </View>
+        <FlatList
+          data={data.forecast}
+          renderItem={({ item }) => (
+            <View>
+              <TextDefault>{item.max}</TextDefault>
+            </View>
+          )}
+          keyExtractor={(item) => item.date}
+          horizontal={true}
+        />
+      </View>
+    </ScrollView>
   );
 }
--- components/StyledComponents.js
+++ components/StyledComponents.js
@@ -3,7 +3,21 @@ import styles from "styled-components";
 const TituloPagina = styles.Text`
     font-size: 20;
 `;
+const TextDefault = styles.Text`
+    font-size: 18;
+`;
+const BgTemperature = styles.Image`
+width: 300px;
+height: 190px;`;
 
-export {
-    TituloPagina,
-};
+const TxtTemperature = styles.Text`
+display: flex;
+font-size: 40;
+color: #fff;
+position: absolute;
+height: 30%;
+top: 35%;
+font-weight: bold;
+justify-content: center`;
+
+export { TituloPagina, TextDefault, BgTemperature, TxtTemperature };
